343fe149a6e8a7f22f93d8c43a333ed7a5a8d59a
Viking <vikingegor@gmail.com>
2017-05-06 02:12:48 +0300

    development Hall continues

---
+++ CatelDemo/Services/Other/Converters/TableToolTipConverter.cs
@@ -0,0 +1,27 @@
+﻿using Catel.MVVM.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantHelper.Services.Other.Converters
+{
+	class TableToolTipConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value != null)
+			{
+				if ((bool)value) return "ДОСТУПЕН";
+			}
+			return "НЕДОСТУПЕН";
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
