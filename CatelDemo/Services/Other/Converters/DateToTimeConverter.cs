6c726945f66fa957407e3c406d925abf5ad43764
Viking <vikingegor@gmail.com>
2017-05-09 06:05:17 +0300

    date to time converter added

---
+++ CatelDemo/Services/Other/Converters/DateToTimeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catel.MVVM.Converters;
+
+namespace RestaurantHelper.Services.Other.Converters
+{
+	class DateToTimeConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return ((DateTime)value).ToShortTimeString();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
