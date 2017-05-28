c63e28775771c748c89fffced34e37bb54a16bb3
Viking <vikingegor@gmail.com>
2017-05-28 15:49:26 +0300

    script for sqlite

--- CatelDemo/SqlScripts/InsertAllTables.sql
+++ CatelDemo/SqlScripts/InsertAllTables.sql
@@ -1,12 +1,12 @@
-﻿SET IDENTITY_INSERT [dbo].[Tables] ON
-INSERT INTO [dbo].[Tables] ([Id], [Number], [SeatsNumber], [Top], [Left], [Type]) VALUES (1, 1, 4, 10, 20, 1)
-INSERT INTO [dbo].[Tables] ([Id], [Number], [SeatsNumber], [Top], [Left], [Type]) VALUES (2, 2, 4, 97, 20, 1)
-INSERT INTO [dbo].[Tables] ([Id], [Number], [SeatsNumber], [Top], [Left], [Type]) VALUES (3, 3, 4, 177, 20, 1)
-INSERT INTO [dbo].[Tables] ([Id], [Number], [SeatsNumber], [Top], [Left], [Type]) VALUES (4, 4, 8, 265, 20, 3)
-INSERT INTO [dbo].[Tables] ([Id], [Number], [SeatsNumber], [Top], [Left], [Type]) VALUES (5, 5, 4, 310, 200, 1)
-INSERT INTO [dbo].[Tables] ([Id], [Number], [SeatsNumber], [Top], [Left], [Type]) VALUES (6, 6, 4, 310, 305, 1)
-INSERT INTO [dbo].[Tables] ([Id], [Number], [SeatsNumber], [Top], [Left], [Type]) VALUES (7, 7, 6, 310, 445, 2)
-INSERT INTO [dbo].[Tables] ([Id], [Number], [SeatsNumber], [Top], [Left], [Type]) VALUES (8, 8, 4, 183, 490, 1)
-INSERT INTO [dbo].[Tables] ([Id], [Number], [SeatsNumber], [Top], [Left], [Type]) VALUES (9, 9, 4, 10, 352, 1)
-INSERT INTO [dbo].[Tables] ([Id], [Number], [SeatsNumber], [Top], [Left], [Type]) VALUES (10, 10, 6, 10, 160, 2)
-SET IDENTITY_INSERT [dbo].[Tables] OFF
+﻿
+insert into Tables (Id, Number, SeatsNumber, Top, Left, Type) values
+  (1, 1, 4, 10, 20, 1),
+  (2, 2, 4, 97, 20, 1),
+  (3, 3, 4, 177, 20, 1),
+  (4, 4, 8, 265, 20, 3),
+  (5, 5, 4, 310, 200, 1),
+  (6, 6, 4, 310, 305, 1),
+  (7, 7, 6, 310, 445, 2),
+  (8, 8, 4, 183, 490, 1),
+  (9, 9, 4, 10, 352, 1),
+  (10, 10, 6, 10, 160, 2)
